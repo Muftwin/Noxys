3c3b7bc89f6e302045c57d10a94f47254e6d08a4
Muftwin <alotocookiesisthebest@gmail.com>
2022-11-09 21:40:42 -0600

    Dash
    Added a simple dash. Press X

--- Player.cs
+++ Player.cs
@@ -3,10 +3,10 @@ using System;
 
 public class Player : Sprite
 {
-	// Declare member variables here. Examples:
-	// private int a = 2;
-	// private string b = "text";
-
+	int facing = 1; //1 for right -1 for left
+	bool dashing = false;
+	float dashTime = 0.25f;
+	
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{	  
@@ -30,11 +30,24 @@ public class Player : Sprite
 		if (Input.IsKeyPressed((int)KeyList.A))
 		{
 			this.Position += new Vector2(-AMOUNT,0);
+			facing = -1;
 		}
 		if (Input.IsKeyPressed((int)KeyList.D))
 		{
 			this.Position += new Vector2(AMOUNT,0);
+			facing = 1;
 		}
-	 
+
+		if (Input.IsKeyPressed((int)KeyList.X)){
+			dashing = true;
+		}
+		if(dashing){
+			this.Position += new Vector2(AMOUNT * facing,0);
+			dashTime -= delta;
+		}
+		if(dashTime<= 0){
+			dashing = false;
+			dashTime = 0.25f;
+		}
  }
 }
