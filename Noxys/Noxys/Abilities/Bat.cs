f9b8d3bbb733f792ae8242ae9163d88dd0133795
Codepheonix0 <108499830+Codepheonix0@users.noreply.github.com>
2022-11-17 19:26:23 -0600

    Add files via upload
    Hitbox for bat added

---
+++ Noxys/Noxys/Abilities/Bat.cs
@@ -0,0 +1,32 @@
+using Godot;
+public class Bat : Ability
+{
+    Player player;
+    bool enabled = true; //allowed to use at all
+    bool currentTransform = false; //currently using
+    public Bat(Player player)
+    {
+        this.player = player;
+    }
+
+    public bool transform()
+    {
+        if (!enabled)
+            return false;
+
+        player.character = Player.Character.BAT;
+        player.setSprite("bat.png");
+        currentTransform = true;
+        return true;
+    }
+    public void passive()
+    {
+        if (!enabled || !currentTransform)
+            return;
+
+        if (Input.IsKeyPressed((int)KeyList.Up) || Input.IsKeyPressed((int)KeyList.W))
+            player.gravity = 1f;
+        else
+            player.gravity = 5f;
+    }
+}
