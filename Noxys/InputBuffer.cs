8c230fc0e6f627c8573b6e1ed58933af44d9c27a
Muftwin <alotocookiesisthebest@gmail.com>
2022-11-22 21:05:31 -0600

    Coyote Time and Quick Turning

--- Noxys/InputBuffer.cs
+++ Noxys/InputBuffer.cs
@@ -8,10 +8,19 @@ public class InputBuffer
 
     public bool usedTheSpaceToJumpAlready = false;
 
+    public float lastOnTheFloor;
+    public float yCoordinateLastOnTheFloor;
+
+    public float leftLastUp;
+    public float rightLastUp;
+    public bool leftMoreRecentThanRight; //Assuming you are pressing left
+
     public void update(float delta)
     {
         spaceLastUp += delta;
         spaceLastDown += delta;
+
+        lastOnTheFloor += delta;
 
         if (Input.IsKeyPressed((int)KeyList.Space))
             spaceLastDown = 0;
@@ -20,6 +29,15 @@ public class InputBuffer
             spaceLastUp = 0;
             usedTheSpaceToJumpAlready = false;
         }
+
+        leftLastUp += delta;
+        rightLastUp += delta;
+        if (!Input.IsKeyPressed((int)KeyList.Left))
+            leftLastUp = 0;
+        if (!Input.IsKeyPressed((int)KeyList.Right))
+            rightLastUp = 0;
+
+        leftMoreRecentThanRight = leftLastUp < rightLastUp;
 
         spaceWasDown = Input.IsKeyPressed((int)KeyList.Space); //do this last so we can check if it changed next time //do i actually need this?
     }
