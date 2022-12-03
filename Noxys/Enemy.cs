5de610044c61c2774042fd4f81675cd1b4f30ada
Muftwin <alotocookiesisthebest@gmail.com>
2022-12-02 19:31:05 -0600

    Buffer and Anti Repeat for dash

--- Noxys/Enemy.cs
+++ Noxys/Enemy.cs
@@ -1,4 +1,4 @@
-	using Godot;
+using Godot;
 using System;
 
 public class Enemy : RigidBody2D
@@ -11,47 +11,47 @@ public class Enemy : RigidBody2D
 	public float gravity = 1;
 	public void jump()
 	{
-		if(!hasjumped)
+		if (!hasjumped)
 		{
-this.ApplyCentralImpulse(new Vector2(0, -300));
-		hasjumped = true;
+			this.ApplyCentralImpulse(new Vector2(0, -300));
+			hasjumped = true;
 		}
 	}
 
-public void Move(float x, float y)
-{
-	
-this.ApplyCentralImpulse(new Vector2(x, y));
-}
-public void _on_Sight_body_entered(PhysicsBody2D body)
-{
-	if(body.Name == "Player")
+	public void Move(float x, float y)
 	{
-		jump();
-	GD.Print("I See you");
-	}
-	}
-public void _on_Enemy_body_entered(PhysicsBody2D body)
-{
-hasjumped = false;
-iscolliding = true;
 
-}
+		this.ApplyCentralImpulse(new Vector2(x, y));
+	}
+	public void _on_Sight_body_entered(PhysicsBody2D body)
+	{
+		if (body.Name == "Player")
+		{
+			jump();
+			GD.Print("I See you");
+		}
+	}
+	public void _on_Enemy_body_entered(PhysicsBody2D body)
+	{
+		hasjumped = false;
+		iscolliding = true;
+
+	}
 	public override void _Ready()
 	{
-		
+
 	}
 
 
-public override void _Process(float delta)
- {
-	if(iscolliding == false)
-			this.ApplyCentralImpulse(new Vector2(0,gravity));
+	public override void _Process(float delta)
+	{
+		if (iscolliding == false)
+			this.ApplyCentralImpulse(new Vector2(0, gravity));
 
-	
-	
-	
- }
+
+
+
+	}
 
 
 
--- Noxys/Player.cs
+++ Noxys/Player.cs
@@ -2,163 +2,152 @@ using System;
 using Godot;
 public class Player : KinematicBody2D
 {
-	[Signal]
-	public delegate void Collide();
-		[Signal]
-	public delegate void Hit();
-public bool dead = false;
-	public int facing = 1; //1 for right -1 for left
-	public Character character = Character.ADOL;
-	public Godot.Sprite sprite;
-	public Vector2 speed = new Vector2();
+    [Signal]
+    public delegate void Collide();
+    [Signal]
+    public delegate void Hit();
 
-	[Export] public int maxWalkSpeed = 350;
-	[Export] int walkIncrement = 10;
-	[Export] int slowIncrement = 50;
-	[Export] public int JumpSpeed = -350;
-	[Export] public float jumpBufferLength = 0.5f;
-	[Export] public float coyoteTimeLength = 7.5f;
+    public bool dead = false;
+    public int facing = 1; //1 for right -1 for left
+    public Character character = Character.ADOL;
+    public Godot.Sprite sprite;
+    public Vector2 speed = new Vector2();
+    public InputBuffer inputBuffer = new InputBuffer();
 
-	public const float INITIAL_GRAVITY = 5f;
-	public float gravity = INITIAL_GRAVITY; //delete this I don't need 2 gravitys
-	public int Gravity = 500;
+    [Export] public int gravity = 500;
+    [Export] public int maxWalkSpeed = 350;
+    [Export] int walkIncrement = 10;
+    [Export] int slowIncrement = 50;
+    [Export] public int JumpSpeed = -350;
+    [Export] public float jumpBufferLength = 0.5f;
+    [Export] public float coyoteTimeLength = 7.5f;
+    [Export] public float dashBufferLength = 0.65f;
 
-	float[] spawn = new float[2];
+    float[] spawn = new float[2];
 
-	Bat bat;
-	Cat cat;
-	Bull bull;
+    Bat bat;
+    Cat cat;
+    Bull bull;
 
-	InputBuffer inputBuffer = new InputBuffer();
+    public enum Character
+    {
+        ADOL, CAT, BAT, BULL, DOLL, WIZARD, SKULL
+    }
 
-	public enum Character
-	{
-		ADOL, CAT, BAT, BULL, DOLL, WIZARD
-	}
+    public override void _Ready()
+    {
+        sprite = this.GetNode<Godot.Sprite>("plrsprite");
 
-	public override void _Ready()
-	{
-		sprite = this.GetNode<Godot.Sprite>("plrsprite");
+        spawn[0] = this.Position.x;
+        spawn[1] = this.Position.y;
 
-		spawn[0] = this.Position.x;
-		spawn[1] = this.Position.y;
+        bat = new Bat(this);
+        cat = new Cat(this);
+        bull = new Bull(this);
+    }
 
-		bat = new Bat(this);
-		cat = new Cat(this);
-		bull = new Bull(this);
-	}
+    public override void _PhysicsProcess(float delta)
+    {
+        inputBuffer.update(delta);
+        if (Input.IsKeyPressed((int)KeyList.R)) //temp reset
+        {
+            this.Position = new Vector2(spawn[0], spawn[1]);
+            setSprite("res://bat.png");
+            dead = false;
+            inputBuffer = new InputBuffer();
+            return;
+        }
 
-	public override void _PhysicsProcess(float delta)
-	{
-		if(dead)
-	{
-		if (Input.IsKeyPressed((int)KeyList.R))
-		{
-			this.Position = new Vector2(spawn[0], spawn[1]);
-			setSprite("res://bat.png");
-			dead = false;
-			return;
-		}
-					return;
-	}
-		inputBuffer.update(delta);
+        if (dead)
+            return;
 
-		bool jump = (inputBuffer.spaceLastDown <= jumpBufferLength * delta && !inputBuffer.usedTheSpaceToJumpAlready && IsOnFloor());
-		bool coyoteJump = (inputBuffer.spaceLastDown <= jumpBufferLength * delta && coyoteTimeLength * delta >= inputBuffer.lastOnTheFloor && this.Position.y >= inputBuffer.yCoordinateLastOnTheFloor);
+        bool jump = (inputBuffer.spaceLastDown <= jumpBufferLength * delta && !inputBuffer.usedTheSpaceToJumpAlready && IsOnFloor());
+        bool coyoteJump = (inputBuffer.spaceLastDown <= jumpBufferLength * delta && coyoteTimeLength * delta >= inputBuffer.lastOnTheFloor && this.Position.y >= inputBuffer.yCoordinateLastOnTheFloor);
 
-		if (jump || coyoteJump)
-		{
-			speed.y = JumpSpeed;
-			inputBuffer.usedTheSpaceToJumpAlready = true;
-		}
+        if (jump || coyoteJump)
+        {
+            speed.y = JumpSpeed;
+            inputBuffer.usedTheSpaceToJumpAlready = true;
+        }
 
-		bool right = Input.IsActionPressed("ui_right");
-		bool left = Input.IsActionPressed("ui_left");
-		if (right && (!left || !inputBuffer.leftMoreRecentThanRight))
-		{
-			if (speed.x >= 0)
-				speed.x = Math.Min(speed.x + walkIncrement, maxWalkSpeed);
-			else //if you were going left slow down faster
-				speed.x = Math.Min(speed.x + slowIncrement, maxWalkSpeed);
-			facing = 1;
-		}
-		if (left && (!right || inputBuffer.leftMoreRecentThanRight))
-		{
-			if (speed.x <= 0)
-				speed.x = Math.Max(speed.x - walkIncrement, -maxWalkSpeed);
-			else
-				speed.x = Math.Max(speed.x - slowIncrement, -maxWalkSpeed);
-			facing = -1;
-		}
-		if (!right && !left)
-		{
-			if (speed.x < 0)
-				speed.x = Math.Min(0, speed.x + slowIncrement);
-			else if (speed.x > 0)
-				speed.x = Math.Max(0, speed.x - slowIncrement);
-		}
+        bool right = Input.IsActionPressed("ui_right");
+        bool left = Input.IsActionPressed("ui_left");
+        if (right && (!left || !inputBuffer.leftMoreRecentThanRight))
+        {
+            if (speed.x >= 0)
+                speed.x = Math.Min(speed.x + walkIncrement, maxWalkSpeed);
+            else //if you were going left slow down faster
+                speed.x = Math.Min(speed.x + slowIncrement, maxWalkSpeed);
+            facing = 1;
+        }
+        if (left && (!right || inputBuffer.leftMoreRecentThanRight))
+        {
+            if (speed.x <= 0)
+                speed.x = Math.Max(speed.x - walkIncrement, -maxWalkSpeed);
+            else
+                speed.x = Math.Max(speed.x - slowIncrement, -maxWalkSpeed);
+            facing = -1;
+        }
+        if (!right && !left)
+        {
+            if (speed.x < 0)
+                speed.x = Math.Min(0, speed.x + slowIncrement);
+            else if (speed.x > 0)
+                speed.x = Math.Max(0, speed.x - slowIncrement);
+        }
 
-		speed.y += Gravity * delta;
-		if (bat.slowFall && speed.y > 0)
-			speed.y /= 1.25f;
+        speed.y += gravity * delta;
+        if (bat.slowFall && speed.y > 0)
+            speed.y /= 1.25f;
 
-		if (IsOnFloor()) //Should this be inside input buffer? rn input buffer doesnt depend on player, but...idk
-		{
-			inputBuffer.lastOnTheFloor = 0;
-			inputBuffer.yCoordinateLastOnTheFloor = this.Position.y;
-		}
+        if (IsOnFloor()) //Should this be inside input buffer? rn input buffer doesnt depend on player, but...idk
+        {
+            inputBuffer.lastOnTheFloor = 0;
+            inputBuffer.yCoordinateLastOnTheFloor = this.Position.y;
+        }
 
-		if (bull.dashing)
-		{
-			speed.y = 0;
-			MoveAndSlide(new Vector2(500 * facing, 0));
-		}
-		else
-		{
-			speed = MoveAndSlide(speed, new Vector2(0, -1)); //gravity
-		}
+        if (bull.dashing)
+        {
+            speed.y = 0;
+            MoveAndSlide(new Vector2(500 * facing, 0));
+        }
+        else
+        {
+            speed = MoveAndSlide(speed, new Vector2(0, -1)); //gravity
+        }
 
-		if (Input.IsKeyPressed((int)KeyList.Up) || Input.IsKeyPressed((int)KeyList.W))
-		{
-			bat.transform();
-		}
-		if (Input.IsKeyPressed((int)KeyList.Shift))
-		{
-			bull.transform();
-		}
+        if (Input.IsKeyPressed((int)KeyList.Up) || Input.IsKeyPressed((int)KeyList.W))
+        {
+            bat.transform();
+        }
+        if (inputBuffer.shiftLastDown <= dashBufferLength /* * delta */ && !inputBuffer.usedTheShiftToDashAlready)
+        {
+            bull.transform();
+        }
 
-		bat.passive();
-		cat.passive();
-		bull.passive(delta);
+        bat.passive();
+        cat.passive();
+        bull.passive(delta);
 
-		//temp reset
-		if (Input.IsKeyPressed((int)KeyList.R))
-		{
-			this.Position = new Vector2(spawn[0], spawn[1]);
-		}
-	}
+        //temp reset
+        if (Input.IsKeyPressed((int)KeyList.R))
+        {
+            this.Position = new Vector2(spawn[0], spawn[1]);
+        }
+    }
 
-	public void setSprite(string path)
-	{
-		sprite.Texture = ResourceLoader.Load(path) as Texture;
-	}
+    public void setSprite(string path)
+    {
+        sprite.Texture = ResourceLoader.Load(path) as Texture;
+    }
 
-
-
-
-
-
-
-
-public void _on_Enemy_body_entered(PhysicsBody2D body)
-{
-if(body.Name == "Player")
-{
-
-	setSprite("res://skeleton.png");
-	GD.Print("Game Over");
-	dead = true;
-	
-}
-}
+    public void _on_Enemy_body_entered(PhysicsBody2D body) //delete this?
+    {
+        if (body.Name == "Player")
+        {
+            setSprite("res://skeleton.png");
+            GD.Print("Game Over");
+            dead = true;
+        }
+    }
 }
