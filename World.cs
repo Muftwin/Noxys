523c31f17c8a8db78b4a00fa63fcc137ee298217
valkyrienyanko <sebastianbelle074@protonmail.com>
2023-01-16 20:32:59 -0600

    change how world is restarted

--- World.cs
+++ World.cs
@@ -9,13 +9,24 @@ public partial class World : Node
 	// private int a = 2;
 	// private string b = "text";
 
+	private PackedScene Scene { get; set; }
+
+	public override void _Ready()
+	{
+		Scene = GD.Load<PackedScene>("res://Scene.tscn");
+
+		AddChild(Scene.Instantiate());
+	}
+
 	public override void _Input(InputEvent @event)
 	{
 		//GD.Print(Engine.GetFramesPerSecond());
 		if (Input.IsKeyPressed(Key.R))
 		{
-			var error = GetTree().ReloadCurrentScene();
-			GD.Print($"Restart: {error}");
+			foreach (Node child in GetChildren())
+				child.QueueFree();
+
+			AddChild(Scene.Instantiate());
 		}
 	}
 }
