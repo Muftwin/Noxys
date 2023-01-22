0f90bf7f814c5dff7b24eef2b174616ee3102b7d
Codepheonix0 <108499830+Codepheonix0@users.noreply.github.com>
2023-01-22 14:55:47 -0600

    Add files via upload

---
+++ TextEdit.cs
@@ -0,0 +1,17 @@
+using Godot;
+using System;
+
+public class TextEdit : Godot.TextEdit
+{
+
+	public override void _Ready()
+	{
+
+
+	}
+
+//  public override void _Process(float delta)
+//  {
+//      
+//  }
+}
