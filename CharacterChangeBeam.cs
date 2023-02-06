e2519bf68485b4653cac10d873238e892b829724
cramps-man <cramps-man@hotmail.com>
2023-02-06 18:12:39 +1100

    Add height export variable

--- CharacterChangeBeam.cs
+++ CharacterChangeBeam.cs
@@ -5,9 +5,19 @@ public partial class CharacterChangeBeam : Area2D
 {
 	[Export]
 	private Player.Character characterToChangeInto;
+	[Export]
+	private int heightpx;
 	
 	public override void _Ready()
 	{
+		update_height_of_beam();
+	}
+	
+	private void update_height_of_beam()
+	{
+		CollisionShape2D cs = (CollisionShape2D)ShapeOwnerGetOwner((uint)GetShapeOwners()[0]);
+		SegmentShape2D shape = (SegmentShape2D)cs.Shape;
+		shape.B = new Vector2(0, -heightpx);
 	}
 	
 	private void _on_body_entered(Node2D body)
